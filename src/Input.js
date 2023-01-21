4e312654b3db111578c86e38a1c973e13f04fc24
mohammad razavi <mohammadrazavi71@gmail.com>
2023-01-21 18:45:58 +0330

    css done / end of the projekt

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { BsEyeFill } from "react-icons/fa";
+import { useState } from 'react';
+import { Icon } from 'react-icons-kit';
+import { eye } from 'react-icons-kit/icomoon/eye';
+
+
+
+import styles from "./Input.module.css"
 
 const Input = () => {
+    const [type,setType] = useState("password");
+    const [icons,setIcons]= useState(eye)
+
+
+    const changeType = ()=> {
+        if(type==="password"){
+            setType("text")
+            setIcons(eye)
+        }else {
+            setType("password")
+            
+        }
+    }
     return (
-        <div >
-            <div>
-                <input type="password" />
+        <div className={styles.container}>
+            <div className={styles.myinput}>
+                <input type={type} />
+                <span onClick={changeType}><Icon icon={icons} /></span>
             </div>
-            <span><BsEyeFill /></span>
+
         </div>
     );
 }
