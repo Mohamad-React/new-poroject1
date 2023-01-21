9d528ef261a58e371eeed799a2294a14cccf0b89
mohammad razavi <mohammadrazavi71@gmail.com>
2023-01-21 19:04:20 +0330

    project done / show password,hidden password

--- src/Input.js
+++ src/Input.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { useState } from 'react';
 import { Icon } from 'react-icons-kit';
 import { eye } from 'react-icons-kit/icomoon/eye';
-
-
+import { view_off } from 'react-icons-kit/ikons/view_off'
 
 import styles from "./Input.module.css"
 
 const Input = () => {
     const [type,setType] = useState("password");
-    const [icons,setIcons]= useState(eye)
+    const [icons,setIcons]= useState(view_off)
 
 
     const changeType = ()=> {
         if(type==="password"){
             setType("text")
-            setIcons(eye)
+            setIcons(view_off)
         }else {
             setType("password")
+            setIcons(eye)
             
         }
     }
